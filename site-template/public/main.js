70a80eb86c6566b354ae88bcb5156b3a1cd6eb36
Brandon <brandonplokker@live.nl>
2024-06-10 17:27:53 +0200

    fix: fix for language route navigation with custom template

--- site-template/public/main.js
+++ site-template/public/main.js
@@ -1,11 +1,11 @@
 export default {
-    defaultTheme: 'dark',
-    iconLinks: [
-      {
-        icon: 'github',
-        href: 'https://github.com/XR-Solutions',
-        title: 'GitHub'
-      },
-    ],
-    lunrLanguages: ['en', 'nl'],    
-  }
+  defaultTheme: "dark",
+  iconLinks: [
+    {
+      icon: "github",
+      href: "https://github.com/XR-Solutions",
+      title: "GitHub",
+    },
+  ],
+  lunrLanguages: ["en", "nl"],
+};
